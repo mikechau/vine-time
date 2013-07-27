779b7c2386e85f332d1ca1b9f19c6f7bc3b287ef
Mike Chau <mchau@iwu.edu>
2013-07-26 22:26:06 -0500

    Add alert to user that they can slide across videos

--- app/models/fb_video.rb
+++ app/models/fb_video.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 class FbVideo
 
-  FB_URL = URI::encode("https://graph.facebook.com/170901143077174?fields=posts.limit(30).fields(source,name,type)&access_token=#{ENV['FB_APP_TOKEN']}")
+  FB_URL = URI::encode("https://graph.facebook.com/170901143077174?fields=posts.limit(100).fields(source,name,type)&access_token=#{ENV['FB_APP_TOKEN']}")
 
   def json_response
     response = open(FB_URL).read
