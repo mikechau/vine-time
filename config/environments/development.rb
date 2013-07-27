fd3f2a7385ba6e323d083e165a03f35e3a5c6bf7
Mike Chau <mchau@iwu.edu>
2013-07-27 04:27:27 -0500

    add fb website type

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,5 +33,5 @@ VineTime::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = true  
 end
