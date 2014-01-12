f6af52c23ae9c37da11941b9698c14403690a526
Mike Chau <mchau@iwu.edu>
2014-01-12 12:38:22 -0600

    add temp api settings (refactor later)

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,7 @@
 class VideosController < ApplicationController
 
+  before_filter :set_headers
+
   caches_action :index, :expires_in => 15.minutes
   caches_action :show, :cache_path => Proc.new { |c| c.params }, :expires_in => 1.hour
 
@@ -9,6 +11,26 @@ class VideosController < ApplicationController
 
   def show
     @video = FbVideo.new.get_video(params[:id])
+  end
+
+  def api_list_all
+    @videos = FbVideo.new.list
+    # request.format = :json
+    render :json => @videos
+  end
+
+  def api_get_one
+    @video = FbVideo.new.get_video(params[:id])
+    render :json => @video
+  end
+
+  private
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ VineTime::Application.routes.draw do
   get 'about', to: 'static_pages#about', as: :about
   get 'contact', to: 'static_pages#contact', as: :contact
 
+  get 'api/v1/videos', to: 'videos#api_list_all', defaults: {format: :json}
+  get 'api/v1/video/:id', to: 'videos#api_get_one', defaults: {format: :json}
 end
